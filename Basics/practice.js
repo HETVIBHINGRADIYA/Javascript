d7550eff8e01c5ac276b74b37c655b94e8d803e6
HETVIBHINGRADIYA <bhingradiyahetvi@gmail.com>
2024-12-24 12:34:31 +0000

    basic

--- Basics/practice.js
+++ Basics/practice.js
@@ -1,1 +1,77 @@
-console.log("Hello!");
+//console.log("Hello!"); //Printing simple statement
+
+//JS variables
+// const accountId=12345; //It does not 
+// let accountEmail = "abc@gmail.com";
+// var accountPassword = "12345";
+// let accountCity = "Surat";
+// //console.log(accountId);
+
+// //Prefer not to use var due to functional scope and block scope
+
+// //accountId="234n";
+// accountEmail="kiya@gmail.com";
+// accountPassword="kiya@123";
+// console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+//JS Operators
+//Arthimetic operators : +,-,*,/,%,**
+
+// let x=8;
+// let y=4;
+// console.log("x =" ,x , "y=" ,y);
+// console.log("x+y =",x+y);
+// console.log("x-y =", x-y);
+// console.log("x*y =",x*y);
+// console.log("x/y =",x/y);
+// console.log("x%y =", x%y);
+// console.log("x ** y =",x**y);
+
+//Unary Operators ++,--
+
+// let x=7;
+// let y=6;
+
+// console.log("x =",x , "y = ",y);
+// console.log("x++ =",x++);
+// console.log("x =",x);
+// console.log("--y =",--y);
+// console.log("y =",y);
+
+//Assingment operators ==,+=,-=,*=,%=,**
+// let x=8;
+// let y=4;
+
+// x+=x;
+// console.log("x =",x);
+// y-=y;
+// console.log("y =",y);
+// x*=x;
+// console.log("x=",x);
+// y%=y;
+// console.log("y=",y);
+// x**=x;
+// console.log("x=",x);
+
+//Comparison operators ==,!=,===,!==,>,>=,<,<=
+// let a=5;
+// let b=3;
+
+// console.log("Comparison operators: ")
+// console.log("a==b =", a ==b);
+// console.log("a!=b =", a!=b);
+// console.log("a===b =", a===b);
+// console.log("a!==b =", a!==b);
+// console.log("a>b =" ,a>b);
+// console.log("a>=b =",a>=b);
+// console.log("a<b =",a<b);
+// console.log("a<=b =",a<=b);
+
+//Logical operators
+let x=8;
+let y=2;
+
+console.log("cond1 && cond2 =" ,x<y && x===8);
+console.log("cond1 || cond2 =", x<y || x===8);
+console.log("!(8<2) = ", !(x<y))
+
